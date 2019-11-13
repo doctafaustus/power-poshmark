0c0e19e20b0efabc5d18347290898369a61758be
doctafaustus <clowd150@gmail.com>
2019-11-13 00:10:25 -0600

    Fix indentation

--- content.js
+++ content.js
@@ -6,35 +6,35 @@ injectScripts();
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
-	document.dispatchEvent(new CustomEvent('pp-event', { 
-		detail: {
-			action: request.action
-		} 
-	}));
+  document.dispatchEvent(new CustomEvent('pp-event', { 
+    detail: {
+      action: request.action
+    } 
+  }));
 
-	document.addEventListener('pp-message', e => {
-		console.log('pp-message', e.detail);
-		chrome.runtime.sendMessage({
-			messageType: 'log',
-			messageText: e.detail.messageText
-		});
-	});
+  document.addEventListener('pp-message', e => {
+    console.log('pp-message', e.detail);
+    chrome.runtime.sendMessage({
+      messageType: 'log',
+      messageText: e.detail.messageText
+    });
+  });
 
 
-	// We need to return true to get around this error: "The message port closed before a response was received."
-	return true; 
+  // We need to return true to get around this error: "The message port closed before a response was received."
+  return true; 
 });
 
 
 // Load core scripts in sequence
 function injectScripts() {
-	console.log('injectScripts')
-	if (!scripts.length) return console.log('Done loading scripts');
-	const s = document.createElement('script');
-	s.src = chrome.runtime.getURL(scripts.shift());
-	s.onload = () => { 
-		s.remove();
-		injectScripts();
-	};
-	document.head.insertAdjacentElement('beforeend', s);
+  console.log('injectScripts')
+  if (!scripts.length) return console.log('Done loading scripts');
+  const s = document.createElement('script');
+  s.src = chrome.runtime.getURL(scripts.shift());
+  s.onload = () => { 
+    s.remove();
+    injectScripts();
+  };
+  document.head.insertAdjacentElement('beforeend', s);
 }
