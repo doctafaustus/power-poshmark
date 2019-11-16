068930327ef3e77417939116440cfcdbd838d1b2
doctafaustus <clowd150@gmail.com>
2019-11-16 17:05:30 -0600

    Update manifest description

--- dashboard-reloader.js
+++
@@ -1,9 +0,0 @@
-var extensionsManager = document.querySelector('extensions-manager');
-var viewManager = extensionsManager.shadowRoot.querySelector('#viewManager');
-var itemsList = viewManager.querySelector('#items-list');
-var poshmarkCard = itemsList.shadowRoot.querySelector('#lkegafbffjiigdfibgpdellgpphlmeca');
-var refreshButton = poshmarkCard.shadowRoot.querySelector('#dev-reload-button');
-
-var interval = setInterval(() => {
-  refreshButton.click();
-}, 1000);
